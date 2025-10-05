90009535d5c15fb9dcba58248a43fffedf8435f2
Jana <jana.skarkova@gmail.com>
2025-10-05 18:51:23 +0200

    pridani nazdarSvete

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/pozdrav/{jmeno}", (string jmeno) => $"Ahoj {jmeno}!");
-app.MapGet("/secti/{a}/{b}", (int a, int b) => $"Vysledek: {a} + {b} = {a + b}");
+app.MapGet("/nazdarSvete", () => "Nazdar světe!");
+app.MapGet("/pozdrav/{jmeno}", (string jmeno) => $"Ahoj, {jmeno}!");
+app.MapGet("/secti/{a}/{b}", (int a, int b) => $"Výsledek sčítání: {a} + {b} = {a + b}");
 
 app.Run();
