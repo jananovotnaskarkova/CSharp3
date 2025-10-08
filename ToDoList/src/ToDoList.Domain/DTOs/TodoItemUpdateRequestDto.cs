5a4a7f128b365156ce430b201f7fd61bbe8810ea
Jana <jana.skarkova@gmail.com>
2025-10-08 12:02:05 +0200

    update ToDoItemUpdateRequestDto

--- ToDoList/src/ToDoList.Domain/DTOs/TodoItemUpdateRequestDto.cs
+++ ToDoList/src/ToDoList.Domain/DTOs/TodoItemUpdateRequestDto.cs
@@ -2,7 +2,7 @@ namespace ToDoList.Domain.DTOs;
 
 using ToDoList.Domain.Models;
 
-public record TodoItemUpdateRequestDto(string Name, string Description, bool IsCompleted)
+public record TodoItemUpdateRequestDto(int Id, string Name, string Description, bool IsCompleted) //let client know the Id
 {
-    public ToDoItem ToDomain() => new() { Name = Name, Description = Description, IsCompleted = IsCompleted };
+    public ToDoItem ToDomain() => new() { ToDoItemId = Id, Name = Name, Description = Description, IsCompleted = IsCompleted };
 }
